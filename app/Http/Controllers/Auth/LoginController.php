1caa32de8b5b64ea512575c80ac2dff70c13a1c7
sepryan <muhamad.sepryan1028@students.unila.ac.id>
2021-06-27 19:32:21 +0700

    init

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+//    public function redirectTo()
+//    {
+//        if (auth()->user()->is_admin == 1) {
+//            return redirect()->route('user.index');
+//        } else {
+//            return redirect()->route('siswa.index');
+//        }
+//    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->is_admin == 1) {
+            return redirect()->route('user.index');
+        } else {
+            return redirect()->route('siswa.index');
+        }
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+//    public function login(Request $request)
+//    {
+//    }
+}
---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Siswa;
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $siswas = Siswa::all();
+        return view('siswa.index', compact('siswas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('siswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->all();
+        Siswa::create($data);
+        return redirect()->route('siswa.index')->with('success', 'behasil menyimpan data siswa');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function show(Siswa $siswa)
+    {
+        //
+        return view('siswa.show', compact('siswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function edit(Siswa $siswa)
+    {
+        //
+        return view('siswa.edit', compact('siswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, Siswa $siswa)
+    {
+        //
+        $siswa->update($request->all());
+        return redirect()->route('siswa.index')->with('success', 'Berhasil mengedit data');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Siswa $siswa)
+    {
+        //
+        $siswa->delete();
+        return redirect()->route('siswa.index')->with('success', 'Berhasil menghapus data');
+    }
+}
